a254406270f94918f20f20318021fb3b45a3ef27
Adam Shaw <arshaw@users.noreply.github.com>
2017-04-25 22:36:47 -0400

    make travis work, hopefully

--- bin/travis-prep.sh
+++ bin/travis-prep.sh
@@ -1,2 +1,45 @@
+#!/usr/bin/env bash
 
-echo "HHHEEEEYYYYYYY"
+# always immediately exit upon error
+set -e
+
+# start in project root
+cd "`dirname $0`/.."
+
+if [[ -f 'fullcalendar-branch.txt' ]]
+then
+	branch=`cat 'fullcalendar-branch.txt'`
+
+	cd "node_modules"
+
+	if [[ -d "fullcalendar/node_modules" ]]
+	then
+		echo "Already a nested fullcalendar git project."
+		echo "Ensuring an up-to-date '$branch' branch."
+
+		cd "fullcalendar"
+		git fetch origin
+		git checkout --quiet "origin/$branch"
+	else
+		echo "Cloning a fresh fullcalendar repo,"
+		echo "initialized on the '$branch' branch."
+
+		rm -rf "fullcalendar"
+		git clone -b "$branch" "https://github.com/fullcalendar/fullcalendar.git"
+		cd "fullcalendar"
+	fi
+
+	# build
+	npm install
+	gulp dist
+else
+
+	if [[ -d "node_modules/fullcalendar/node_modules" ]]
+	then
+		echo "Removing the nested fullcalendar git project,"
+		echo "and using the proper fullcalendar dependency."
+
+		rm -rf "node_modules/fullcalendar"
+		npm install
+	fi
+fi
